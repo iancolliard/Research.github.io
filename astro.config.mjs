b4764f2a643cb9d66eafdc2dddea904c352ed14a
iancolliard <ian.colliard1@gmail.com>
2025-09-14 22:32:21 -0700

    y

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://iancolliard.github.io/Research.github.io', // full public URL
-  base: '/Research.github.io',                              // leading + trailing slash
+  base: '',                              // leading + trailing slash
   integrations: [sitemap(), mdx(), lit(), icon()],
 });
