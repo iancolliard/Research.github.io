12dadc732e94a84f07740c6a59d065d59c063963
iancolliard <ian.colliard1@gmail.com>
2025-09-17 22:20:28 -0700

    y

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,6 +3,6 @@ export default {
 	description: `Welcome to my website`,
 	url: `https://iancolliard.github.io/Research.github.io`, // No trailing slash!
 	name: `Colliard`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-	enableThemeSwitcher: true,
+	enableThemeSwitcher: false,
 	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
